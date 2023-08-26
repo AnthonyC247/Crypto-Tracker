3bd5056ce7ee6ceb397dba00fa0dcf78b9617998
Anthony Carmona <ac5891358@gmail.com>
2023-08-25 21:34:44 -0700

    making the header somewhat appealing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,34 @@
-import {AppBar, Container, Toolbar, Typography } from "@material-ui/core"; 
+import {AppBar, Container, Select, Toolbar, Typography } from "@material-ui/core"; 
 import React from 'react'
 
+const useStyles = makeStyles(() => ({
+    title: {
+        flex: 1,
+        color: "black",
+        fontFamily: "Open Sans",
+        fontWeight: "bold",
+        cursor: "pointer",
+    }
+}))
+
 const Header = () => {
+
+    const classes = useStyles()
     return (
         <AppBar color="primary" position="static">
             <Container>
                 <Toolbar> 
-                    <Typography>CRYPTO-TRACKER</Typography>
+                    <Typography className={classes.title}>CRYPTO-TRACKER</Typography>
+
+                    <Select variant='outlined' style={{
+                        width: 100,
+                        height: 40,
+                        marginRight: 20,
+
+                    }}>
+                        <MenuItem value={"USD"}>USD</MenuItem>
+                        <MenuItem> value={"EUR"}EUR</MenuItem>
+                    </Select>
                 </Toolbar>
             </Container>
         <AppBar>
