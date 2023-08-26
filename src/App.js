17204babe92031a0b60ecd6a6f0e6a74f1c64841
Anthony Carmona <ac5891358@gmail.com>
2023-08-25 23:34:54 -0700

    Fixing packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React from 'react'; // Don't forget to import React
 import './App.css';
 
-
 function App() {
-  return <div className="App">Hello World</div>;
+  const data = {
+    bitcoin: { usd: 45000 }, // Example data, replace with actual data
+    ethereum: { usd: 3200 }, // Example data, replace with actual data
+    ripple: { usd: 1.2 },    // Example data, replace with actual data
+  };
+
+  return (
+    <div className="App">
+      <h1>Crypto Price Tracker</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Cryptocurrency</th>
+            <th>Price (USD)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bitcoin (BTC)</td>
+            <td>{data['bitcoin']['usd']}</td>
+          </tr>
+          <tr>
+            <td>Ethereum (ETH)</td>
+            <td>{data['ethereum']['usd']}</td>
+          </tr>
+          <tr>
+            <td>Ripple (XRP)</td>
+            <td>{data['ripple']['usd']}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-  
 export default App;
